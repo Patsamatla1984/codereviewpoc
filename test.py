b3604f32346b1e8bb35fff8425c935e64d26c731
Ravindra Varma <ravindra.varma@aveva.com>
2025-06-23 11:38:30 +0530

    comments added

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 
 #this script is to test GPU
+#added
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU'))) 
 
 if tf.test.is_gpu_available(): 
